49a32dde2a3c261c131b9e03723a38ac454af090
TyDennis <46075417+TatendaTy@users.noreply.github.com>
2023-08-10 21:54:51 +0300

    upto accordion

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-import "swiper/css"
+import "swiper/css";
 import './Residencies.css';
 import data from '../../utils/slider.json';
+import { sliderSettings } from '../../utils/common';
 
 const Residencies = () => {
     return (
         <section className="r-wrapper">
-            Residencies
+            <div className="paddings innerWidth r-container">
+                <div className="r-head flexColStart">
+                    <span className="orangeText">Best Choices</span>
+                    <span className="primaryText">Popular Apartments</span>
+                </div>
+
+                {/* destructure as the props in the parent component */}
+                <Swiper {...sliderSettings}>
+                    <SliderButtons />
+                    {
+                        data.map((card, i) => (
+                            <SwiperSlide key={i}>
+                                <div className="flexColStart r-card">
+                                    <img src={card.image} alt="home" />
+
+                                    <span className="secondaryText r-price">
+                                        <span style={{ color: "orange" }}>Ksh</span><span>{card.price} /month</span>
+                                    </span>
+
+                                    <span className="primaryText">{card.name}</span>
+                                    <span className="secondaryText">{card.detail}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+            </div>
         </section>
     )
 }
+
+const SliderButtons = () => { // this is a prev/next button component
+    const swiper = useSwiper();
+    return (
+        <div className="flexCenter r-buttons">
+            <button onClick={() => swiper.slidePrev()}>&lt;</button>
+            <button onClick={() => swiper.slideNext()}>&gt;</button>
+        </div>
+    );
+};
 
 export default Residencies;
